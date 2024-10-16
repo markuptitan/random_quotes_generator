38a366017e64b16597da7cd453d2b1a5b60e08a6
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-10-16 22:03:56 +0200

    Fix formatting and use destructuring

--- js/index.js
+++ js/index.js
@@ -1,71 +1,71 @@
 const categories = [
-  'age',
-  'alone',
-  'amazing',
-  'anger',
-  'architecture',
-  'art',
-  'attitude',
-  'beauty',
-  'best',
-  'birthday',
-  'business',
-  'car',
-  'change',
-  'communication',
-  'computers',
-  'cool',
-  'courage',
-  'dad',
-  'dating',
-  'death',
-  'design',
-  'dreams',
-  'education',
-  'environmental',
-  'equality',
-  'experience',
-  'failure',
-  'faith',
-  'family',
-  'famous',
-  'fear',
-  'fitness',
-  'food',
-  'forgiveness',
-  'freedom',
-  'friendship',
-  'funny',
-  'future',
-  'god',
-  'good',
-  'government',
-  'graduation',
-  'great',
-  'happiness',
-  'health',
-  'history',
-  'home',
-  'hope',
-  'humor',
-  'imagination',
-  'inspirational',
-  'intelligence',
-  'jealousy',
-  'knowledge',
-  'leadership',
-  'learning',
-  'legal',
-  'life',
-  'love',
-  'marriage',
-  'medical',
-  'men',
-  'mom',
-  'money',
-  'morning',
-  'movies',
-  'success'
+  "age",
+  "alone",
+  "amazing",
+  "anger",
+  "architecture",
+  "art",
+  "attitude",
+  "beauty",
+  "best",
+  "birthday",
+  "business",
+  "car",
+  "change",
+  "communication",
+  "computers",
+  "cool",
+  "courage",
+  "dad",
+  "dating",
+  "death",
+  "design",
+  "dreams",
+  "education",
+  "environmental",
+  "equality",
+  "experience",
+  "failure",
+  "faith",
+  "family",
+  "famous",
+  "fear",
+  "fitness",
+  "food",
+  "forgiveness",
+  "freedom",
+  "friendship",
+  "funny",
+  "future",
+  "god",
+  "good",
+  "government",
+  "graduation",
+  "great",
+  "happiness",
+  "health",
+  "history",
+  "home",
+  "hope",
+  "humor",
+  "imagination",
+  "inspirational",
+  "intelligence",
+  "jealousy",
+  "knowledge",
+  "leadership",
+  "learning",
+  "legal",
+  "life",
+  "love",
+  "marriage",
+  "medical",
+  "men",
+  "mom",
+  "money",
+  "morning",
+  "movies",
+  "success",
 ];
 
 function getRandomCategory() {
@@ -74,45 +74,49 @@ function getRandomCategory() {
 }
 
 const fetchQuote = (category) => {
-  const apiKey = 'VqX5koegh8IRelt4umtTWA==IVC0lk1bsYeyyfoS';
+  const apiKey = "VqX5koegh8IRelt4umtTWA==IVC0lk1bsYeyyfoS";
   const requestURL = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
-  
-  fetch(requestURL, {
-    method: 'GET',
-    headers: {
-      'X-Api-Key': apiKey,
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    const quoteText = data[0]?.quote || 'No quote found';
-    const quoteAuthor = data[0]?.author || 'Unknown author';
-    document.getElementById('quote').innerHTML = `"${quoteText}"<br><strong>- ${quoteAuthor}</strong>`;
-  })
-  .catch(error => {
-     Swal.fire({
-      title: 'Error!',
-      text: 'There was an issue fetching the quote. Please try again later.',
-      icon: 'error',
-      confirmButtonText: 'OK'
-    });
-    console.error('There has been a problem with your fetch operation:', error);
-  });
-}
 
-document.addEventListener('DOMContentLoaded', () => {
+  fetch(requestURL, {
+    method: "GET",
+    headers: {
+      "X-Api-Key": apiKey,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const { quoteText, quoteAuthor } = data;
+      document.getElementById(
+        "quote"
+      ).innerHTML = `"${quoteText}"<br><strong>- ${quoteAuthor}</strong>`;
+    })
+    .catch((error) => {
+      Swal.fire({
+        title: "Error!",
+        text: "There was an issue fetching the quote. Please try again later.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+    });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
   const category = getRandomCategory();
   fetchQuote(category);
 });
 
-document.getElementById('generate').addEventListener('click', () => {
+document.getElementById("generate").addEventListener("click", () => {
   const category = getRandomCategory();
   fetchQuote(category);
 });
